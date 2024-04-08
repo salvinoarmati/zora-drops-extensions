2508e5817396c8b69f79ab0c417b27b2ba27f5ec
jalil <jalil@1001.digital>
2024-04-08 18:28:43 +0200

    copy and rename nouns-vision

---
+++ src/oviators/ERC721DropMinterInterface.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface ERC721DropMinterInterface {
+    function adminMint(address recipient, uint256 quantity)
+        external
+        returns (uint256);
+}
