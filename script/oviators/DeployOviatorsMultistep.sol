d162312d723ef51fa680f53afa79c62cc37b77c3
signoraArmati <signora@nounsvision.com>
2024-04-08 14:09:06 -0400

    Comment out disco-specific deploy steps

--- script/oviators/DeployOviatorsMultistep.sol
+++ script/oviators/DeployOviatorsMultistep.sol
@@ -26,49 +26,49 @@ contract DeployOviators is Script {
         address newAdminAddress;
         address nounsDiscoAddress;
         address nounsDiscoRedeemedAddress;
-        address swapMinterAddress;
+        // address swapMinterAddress;
     }
 
     uint64 public constant MAX_DISCO_SUPPLY = 500;
     uint256 public constant PRICE_PER_DISCO = 0.19 ether;
 
     function run() external {
-        // step 1 create a a Nouns Vision Disco drop
-        // step 2 grant nouns vision disco redeemed admin to deployer address
-        // step 2a: important: keep your admin access from another address. deployer will revoke
-        // step 3 create a a Nouns Vision Disco Redeemed drop
-        // step 3a: important: keep your admin access from another address. deployer will revoke
-        // step 4 grant nouns vision disco admin to deployer address
+    //     // step 1 create a a Nouns Vision Disco drop
+    //     // step 2 grant nouns vision disco redeemed admin to deployer address
+    //     // step 2a: important: keep your admin access from another address. deployer will revoke
+    //     // step 3 create a a Nouns Vision Disco Redeemed drop
+    //     // step 3a: important: keep your admin access from another address. deployer will revoke
+    //     // step 4 grant nouns vision disco admin to deployer address
 
         Addresses memory adrs = Addresses({
             deployer: payable(vm.envAddress("deployer")),
-            nounsTokenAddress: vm.envAddress("nouns_token"),
+            // nounsTokenAddress: vm.envAddress("nouns_token"),
             newAdminAddress: vm.envAddress("new_admin_address"),
-            nounsDiscoAddress: vm.envAddress("nouns_vision_disco"),
-            nounsDiscoRedeemedAddress: vm.envAddress("nouns_vision_redeemed"),
-            swapMinterAddress: vm.envAddress("swap_minter_address")
+            nounsDiscoAddress: vm.envAddress("nouns_vision_disco"), // Token before redemption
+            nounsDiscoRedeemedAddress: vm.envAddress("nouns_vision_redeemed"), // Token after redemption
+            // swapMinterAddress: vm.envAddress("swap_minter_address")
         });
 
         vm.startBroadcast(adrs.deployer);
 
-        if (adrs.swapMinterAddress == address(0)) {
-            // 3 setup the ERC721OviatorsExchangeSwapMinter (standalone contract that takes nouns and nouns vision contracts)
-            // set minter for NOUNS_VISION_DISCO as ERC721OviatorsExchangeSwapMinter contract
-            adrs.swapMinterAddress = address(new ERC721OviatorsExchangeSwapMinter({
-                _nounsToken: adrs.nounsTokenAddress,
-                _discoGlasses: adrs.nounsDiscoAddress,
-                _maxAirdropCutoffNounId: 200,
-                _costPerNoun: PRICE_PER_DISCO,
-                _initialOwner: adrs.newAdminAddress,
-                _claimPeriodEnd: 1677474000
-            }));
+    //     if (adrs.swapMinterAddress == address(0)) {
+    //         // 3 setup the ERC721OviatorsExchangeSwapMinter (standalone contract that takes nouns and nouns vision contracts)
+    //         // set minter for NOUNS_VISION_DISCO as ERC721OviatorsExchangeSwapMinter contract
+    //         adrs.swapMinterAddress = address(new ERC721OviatorsExchangeSwapMinter({
+    //             _nounsToken: adrs.nounsTokenAddress,
+    //             _discoGlasses: adrs.nounsDiscoAddress,
+    //             _maxAirdropCutoffNounId: 200,
+    //             _costPerNoun: PRICE_PER_DISCO,
+    //             _initialOwner: adrs.newAdminAddress,
+    //             _claimPeriodEnd: 1677474000
+    //         }));
 
-            ERC721Drop nounsDiscoDrop = ERC721Drop(
-                payable(adrs.nounsDiscoAddress)
-            );
-            bytes32 minterRole = nounsDiscoDrop.MINTER_ROLE();
-            nounsDiscoDrop.grantRole(minterRole, adrs.swapMinterAddress);
-        }
+    //         ERC721Drop nounsDiscoDrop = ERC721Drop(
+    //             payable(adrs.nounsDiscoAddress)
+    //         );
+    //         bytes32 minterRole = nounsDiscoDrop.MINTER_ROLE();
+    //         nounsDiscoDrop.grantRole(minterRole, adrs.swapMinterAddress);
+    //     }
 
         // 4 setup the OviatorsExchangeMinterModule
         //  from token = NOUNS_VISION_DISCO // to token = DISCO_VISION_REDEEMED
@@ -92,18 +92,18 @@ contract DeployOviators is Script {
                 "0xcafe"
             );
 
-        // Can be hard-coded into the drop with data from @salvino, can also be done via etherscan
-        OviatorsExchangeMinterModule.ColorSetting[]
-            memory colorSettings = new OviatorsExchangeMinterModule.ColorSetting[](
-                1
-            );
-        colorSettings[0] = OviatorsExchangeMinterModule.ColorSetting({
-            color: "disco",
-            maxCount: MAX_DISCO_SUPPLY,
-            animationURI: "",
-            imageURI: ""
-        });
-        exchangeMinterModule.setColorLimits(colorSettings);
+    //     // Can be hard-coded into the drop with data from @salvino, can also be done via etherscan
+    //     OviatorsExchangeMinterModule.ColorSetting[]
+    //         memory colorSettings = new OviatorsExchangeMinterModule.ColorSetting[](
+    //             1
+    //         );
+    //     colorSettings[0] = OviatorsExchangeMinterModule.ColorSetting({
+    //         color: "disco",
+    //         maxCount: MAX_DISCO_SUPPLY,
+    //         animationURI: "",
+    //         imageURI: ""
+    //     });
+    //     exchangeMinterModule.setColorLimits(colorSettings);
 
         if (adrs.newAdminAddress != adrs.deployer) {
             ERC721Drop(payable(adrs.nounsDiscoAddress)).revokeRole(
